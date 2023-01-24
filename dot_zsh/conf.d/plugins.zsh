a29ff8ae9e018e5afe73ef9ffe53389524120cd2
Hess Lewis <hrl@hrlou.net>
2023-01-24 12:01:15 -0500

    Add .chezmoiexternal.toml Add .zsh/conf.d/plugins.zsh Add .zsh/conf.d/wsl.zsh

---
+++ dot_zsh/conf.d/plugins.zsh
@@ -0,0 +1,2 @@
+ZSH_HIGHLIGHT_DIRS_BLACKLIST+=(/mnt/c)
+source $ZDOTDIR/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
---
+++ dot_zsh/conf.d/wsl.zsh
@@ -0,0 +1,4 @@
+[[ "$(uname -r)" =~ "WSL" ]] || return
+# WSL Specific
+# I don't like adding windows programmes to path
+alias code="/mnt/c/Program\ Files/Microsoft\ VS\ Code/bin/code"
