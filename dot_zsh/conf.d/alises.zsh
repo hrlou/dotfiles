58d4666a838abd2916c717c77a8a2fcce0f0d0fa
Hess Lewis <hrl@hrlou.net>
2023-01-24 10:56:44 -0500

    update

---
+++ dot_zsh/conf.d/alises.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+alias ls='ls --color=auto'
+alias ll='ls -ahlF --color=auto'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias grep='grep --color=auto'
+
+alias h='fc -l'
+alias j=jobs
+alias m=$PAGER
+alias g='egrep -i'
