e862f2c9887f6df9e92f8127047f6dee5a6bb195
Hess Lewis <hrl@hrlou.net>
2023-01-26 08:47:18 -0500

    Update .zsh/conf.d/alises.zsh

--- dot_zsh/conf.d/alises.zsh
+++ dot_zsh/conf.d/alises.zsh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
+alias cls='clear'
+alias cz='chezmoi'
 alias ls='ls --color=auto'
 alias ll='ls -ahlF --color=auto'
 alias la='ls -A'
 alias l='ls -CF'
-
 alias grep='grep --color=auto'
-
 alias h='fc -l'
 alias j=jobs
 alias m=$PAGER
