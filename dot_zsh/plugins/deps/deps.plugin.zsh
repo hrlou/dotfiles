1cd6a2b272654cd1d19444f80f9cbcc8b6485e83
Hess Lewis <hrl@hrlou.net>
2023-01-31 09:52:49 -0500

    Add .zsh/plugins/deps/deps.plugin.zsh

---
+++ dot_zsh/plugins/deps/deps.plugin.zsh
@@ -0,0 +1,42 @@
+#!/usr/bin/env zsh
+
+function deps {
+	[ -z "$1" ] && echo "unkown operation" && return
+	
+	if ! (($DEPS[(Ie)$2])); then
+	 	echo "no such dependency"
+		return
+	fi
+
+	deps::load
+
+	if [[ "$1" == "init" ]]; then
+		eval "deps::check::$2" && eval "deps::init::$2"
+	else
+		eval "deps::$1::$2"
+	fi
+}
+
+# load dep files
+function deps::load {
+	for i in $DEPS; do
+		source "$ZDOTDIR/deps.d/$i.dep.zsh"
+	done
+	unset i
+}
+
+# clean unnecessary functions
+function deps::clean {
+	for i in $DEPS; do
+		unset -f deps::{check,install,init}::$i
+	done
+	unset i
+}
+
+
+deps::load
+for i in $DEPS_ENABLED; do
+	deps init $i
+done
+unset i
+deps::clean
