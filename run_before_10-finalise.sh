51b2ff39f59ef45a7a606cc94f83104695a4596e
Hess Lewis <hrl@hrlou.net>
2023-01-31 13:25:39 -0500

    Remove _run_before_02-config.sh Remove .zsh/deps.d/nala.dep.zsh Remove .zsh/deps.d/rust.dep.zsh Remove .zsh/deps.d/starship.dep.zsh Update .zsh/.zshenv Update .zsh/.zshrc Remove .zsh/plugins/deps/deps.plugin.zsh Change attributes of 01-install-manual.sh Change attributes of 10-finalise.sh

--- run_before_10-finalise.sh
+++ run_before_10-finalise.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+_shell=$(awk -F: -v user="$USER" '$1 == user {print $NF}' /etc/passwd)
+[ "$_shell" != "$(which zsh)" ] && sudo chsh -s $(which zsh) "$USER"
 
 mkdir -p $HOME/.local/{bin,share}
 if [ -f "$HOME/bin/chezmoi" ]; then
