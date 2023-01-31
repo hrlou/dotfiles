6d4e8ccf7146791570bfd4f879d9508005790746
Hess Lewis <hrl@hrlou.net>
2023-01-31 14:29:20 -0500

    install script

--- .assets/install.sh
+++ .assets/install.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-echo "Hello, World!"
+# Install Chezmoi
+export BINDIR="$HOME/.local/bin"
+mkdir -p "$BINDIR"
+curl -fsLS get.chezmoi.io | sh
+CZ="$BINDIR/chezmoi"
+
+# Setup
+ssh git@git.hrlou.net || printf "Host git.hrlou.net\n\tHostname git.hrlou.net\n\tUser git\n\tPort 2052\n" >> ~/.ssh/config
+REMOTE="$(ssh git@git.hrlou.net && \
+	printf "git@git.hrlou.net:hrlou/dotfiles.git" || \
+	printf "https://git.hrlou.net/hrlou/dotfiles.git")"
+
+$CZ init $REMOTE
+$CZ update
--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Installation
 
+It's as simple as
+
 ```sh
-sh -c "$(curl -fsLS get.chezmoi.io)"
-ssh git@git.hrlou.net || printf "Host git.hrlou.net\n\tHostname git.hrlou.net\n\tUser git\n\tPort 2052\n" >> ~/.ssh/config
-chezmoi init $(ssh git@git.hrlou.net && printf "git@git.hrlou.net:hrlou/dotfiles.git" || printf "https://git.hrlou.net/hrlou/dotfiles.git")
-chezmoi update
+sh -c "$(curl -fsLS get.hrlou.net/dotfiles)"
 ```
