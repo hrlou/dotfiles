985310f3ef5499305f2f4db72d2f7d8469384964
Hess Lewis <hrl@hrlou.net>
2023-01-31 12:40:30 -0500

    update run-before

---
+++ run_before_01-clean.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# bin
+mkdir -p $HOME/.local/{bin,share}
+if [ -f "$HOME/bin/chezmoi" ]; then
+	mv "$HOME/bin/chezmoi" "$HOME/.local/bin/"
+	[ "$(ls -A "$HOME/bin")" ] || rm -r "$HOME/bin"
+fi	
