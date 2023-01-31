125ccfde5782f2aef8d5d6679a90b2cdfc1f4df4
Hess Lewis <hrl@hrlou.net>
2023-01-31 16:43:51 -0500

    Update 02-install-manual.sh

--- run_before_02-install-manual.sh
+++ run_before_02-install-manual.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if ! command -v starship >/dev/null; then
-	curl --proto '=https' --tlsv1.2 -sSf https://starship.rs/install.sh | sudo sh
+	curl -sS https://starship.rs/install.sh | sudo sh -s -- -y >/dev/null
 fi
