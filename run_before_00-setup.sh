d6e675bc0f0cc139738d3f772439a61c81b4317c
Hess Lewis <hrl@hrlou.net>
2023-01-31 15:54:50 -0500

    update

--- run_before_00-setup.sh
+++ run_before_00-setup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # just incase you aren't prompted this first time
-$HOME/.local/bin/chezmoi init
+# $HOME/.local/bin/chezmoi init
--- run_before_99-finalise.sh
+++ run_before_99-finalise.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 _shell=$(awk -F: -v user="$USER" '$1 == user {print $NF}' /etc/passwd)
 [ "$_shell" != "$(which zsh)" ] && sudo chsh -s $(which zsh) "$USER"
-
-mkdir -p $HOME/.local/{bin,share}
-if [ -f "$HOME/bin/chezmoi" ]; then
-	mv "$HOME/bin/chezmoi" "$HOME/.local/bin/"
-	[ "$(ls -A "$HOME/bin")" ] || rm -r "$HOME/bin"
-fi
-
-$HOME/.local/bin/chezmoi init
