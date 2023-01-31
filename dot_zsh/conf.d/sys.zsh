4196ee0faf7e351ab2a68940b39e99e1dcd778d2
Hess Lewis <hrl@hrlou.net>
2023-01-31 09:31:03 -0500

    Add .zsh/conf.d/sys.zsh

---
+++ dot_zsh/conf.d/sys.zsh
@@ -0,0 +1,27 @@
+#!/usr/bin/env zsh
+# vim: ft=zsh
+
+# SUPER #
+(( $+commands[su] )) && SUPER="su - -c"
+(( $+commands[sudo] )) && SUPER="sudo"
+(( $+commands[doas] )) && SUPER="doas"
+export SUPER="$(which "$SUPER")"
+[[ "$OSTYPE" == linux-android ]] && unset SUPER
+# SUPER #
+
+# PKG #
+
+if (( $+commands[dpkg] )); then
+	PKG="dpkg"
+	(( $+commands[apt] )) && PKG="apt"
+	(( $+commands[apt-get] )) && PKG="apt-get"
+	(( $+commands[nala] )) && PKG="nala"
+elif (( $+commands[rpm] )); then
+	PKG="rpm"
+	(( $+commands[yum] )) && PKG="yum" 
+	(( $+commands[dnf] )) && PKG="dnf"
+	(( $+commands[zypper] )) && PKG="zypper"
+fi
+
+export PKG="$(which "$PKG")"
+# PKG #
