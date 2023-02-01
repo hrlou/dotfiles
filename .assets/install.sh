e7265923625bf0581abb7eb832d1decfc7b07a74
Hess Lewis <hrl@hrlou.net>
2023-01-31 21:20:35 -0500

    love me

--- .assets/install.sh
+++ .assets/install.sh
@@ -5,17 +5,21 @@ mkdir -p "$BINDIR"
 curl -fsLS get.chezmoi.io | sh
 CZ="$BINDIR/chezmoi"
 
+cz_install() {
+	$CZ init $REMOTE
+	$CZ update
+	$CZ init
+	$CZ apply
+}
+
 # Setup
 ssh git@git.hrlou.net 2>/dev/null || printf "Host git.hrlou.net\n\tHostname git.hrlou.net\n\tUser git\n\tPort 2052\n" >> $HOME/.ssh/config
 REMOTE="$(ssh git@git.hrlou.net 2>/dev/null && \
 	printf "git@git.hrlou.net:hrlou/dotfiles.git" || \
 	printf "https://git.hrlou.net/hrlou/dotfiles.git")"
-
-$CZ git remote add origin "$REMOTE"
-$CZ init
-#$CZ init $REMOTE
-$CZ update
-$CZ apply
+while ! $CZ verify; do
+	cz_install
+done
 
 echo "Dotfiles are installed"
 echo "Please restart shell session"
