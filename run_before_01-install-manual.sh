6a9bd4c3f2ee9ba3d8c95b1b87ebfdfad4e56004
Hess Lewis <hrl@hrlou.net>
2023-02-01 15:11:08 -0500

    fix

--- run_before_01-install-manual.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-if ! command -v starship >/dev/null; then
-	curl -sS https://starship.rs/install.sh | sudo sh -s -- -y >/dev/null
-fi
--- run_before_02-finalise.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-_shell=$(awk -F: -v user="$USER" '$1 == user {print $NF}' /etc/passwd)
-[ "$_shell" != "$(which zsh)" ] && sudo chsh -s $(which zsh) "$USER"
-# this stops errors, do not remove
-exit 0
