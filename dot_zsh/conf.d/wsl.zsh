0cad0936fa4581ee5c558aaa297d15c803fa3710
Hess Lewis <hrl@hrlou.net>
2023-02-04 08:53:41 -0500

    winget

--- dot_zsh/conf.d/wsl.zsh
+++ dot_zsh/conf.d/wsl.zsh
@@ -2,3 +2,4 @@
 # WSL Specific
 # I don't like adding windows programmes to path
 alias code="/mnt/c/Program\ Files/Microsoft\ VS\ Code/bin/code"
+alias winget"$HOME/win/AppData/Local/Microsoft/WindowsApps/winget.exe"
