b6ffad49257d33b07bf5b038829b0f3ec828f5f2
Hess Lewis <hrl@hrlou.net>
2023-02-05 13:02:29 -0500

    Update .zsh/conf.d/wsl.zsh

--- dot_zsh/conf.d/wsl.zsh
+++ dot_zsh/conf.d/wsl.zsh
@@ -1,5 +1,5 @@
 [[ "$(uname -r)" =~ "WSL" ]] || return
 # WSL Specific
 # I don't like adding windows programmes to path
-alias code="/mnt/c/Program\ Files/Microsoft\ VS\ Code/bin/code"
+#alias code="/mnt/c/Program\ Files/Microsoft\ VS\ Code/bin/code"
 alias winget"$HOME/win/AppData/Local/Microsoft/WindowsApps/winget.exe"
