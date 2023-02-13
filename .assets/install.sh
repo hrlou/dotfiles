b67b6f65020b015e53af1326c3573ea2d91c21a7
Hess Lewis <hrl@hrlou.net>
2023-02-13 15:25:39 -0500

    Bootstrap installer

--- .assets/install.sh
+++ .assets/install.sh
@@ -1,27 +1,25 @@
 #!/bin/sh
+# Bootstrap
+eval "$(curl -fsLS git.hrlou.net/hrlou/dotfiles/-/raw/main/.assets/include.sh)"
 # Install Chezmoi
 if command -v chezmoi >/dev/null; then
-	echo "Chezmoi found"
-	CZ="$(which chezmoi)"
+        _log_info "chezmoi found"
+        CZ="$(which chezmoi)"
 else
-	echo "Installing chezmoi"
-	export BINDIR="$HOME/.local/bin"
-	mkdir -p "$BINDIR"
-	curl -fsLS get.chezmoi.io | sh
-	CZ="$BINDIR/chezmoi"
+        _log "installing chezmoi"
+        export BINDIR="$HOME/.local/bin"
+        mkdir -p "$BINDIR"
+        curl -fsLS get.chezmoi.io | sh
+        CZ="$BINDIR/chezmoi"
 fi
-
-echo "Testing connection..."
+_log_info "testing connection..."
 REMOTE="$(ssh git@git.hrlou.net >/dev/null 2>&1 && \
-	printf "git@git.hrlou.net:hrlou/dotfiles.git" || \
-	printf "https://git.hrlou.net/hrlou/dotfiles.git")"
-echo "Using '$REMOTE'"
+        printf "git@git.hrlou.net:hrlou/dotfiles.git" || \
+        printf "https://git.hrlou.net/hrlou/dotfiles.git")"
+_log "using '$REMOTE'"
 $CZ init $REMOTE
-$CZ update
-$CZ init
 $CZ apply
 SRC="$(${CZ} source-path)"
 . "${SRC}/.assets/include.sh"
-
 _log "dotfiles are installed"
 _log "please restart shell session"
