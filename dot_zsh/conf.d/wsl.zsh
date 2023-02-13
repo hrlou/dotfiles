08818c0841e2db4b115a289f5afec2418ff0dc40
Hess Lewis <hrl@hrlou.net>
2023-02-13 16:45:34 -0500

    Update .zsh/conf.d/wsl.zsh

--- dot_zsh/conf.d/wsl.zsh
+++ dot_zsh/conf.d/wsl.zsh
@@ -6,5 +6,9 @@ if [ -f "$WINGET" ]; then
 fi
 unset WINGET
 
-WIN_HOME="$(cmd.exe /c "<nul set /p=%UserProfile%" 2>/dev/null)"
+(( $+commands[cmd.exe] )) && CMD="$(which cmd.exe)" || CMD="/mnt/c/Windows/system32/cmd.exe"
+export CMD
+
+WIN_HOME="$(${CMD} /c "<nul set /p=%UserProfile%" 2>/dev/null)"
 WIN_HOME="$(wslpath ${WIN_HOME})"
+export WIN_HOME
