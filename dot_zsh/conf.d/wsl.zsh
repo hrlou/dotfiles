b706b8a2a3878e46bbf9b77c22cd0adcce6e40a0
Hess Lewis <hrl@hrlou.net>
2023-02-13 20:30:48 -0500

    Remove win symlink (create in zsh)

--- dot_zsh/conf.d/wsl.zsh
+++ dot_zsh/conf.d/wsl.zsh
@@ -4,11 +4,12 @@ export WINGET="$HOME/win/AppData/Local/Microsoft/WindowsApps/winget.exe"
 if [ -f "$WINGET" ]; then
 	alias winget="$WINGET"
 fi
-unset WINGET
 
 (( $+commands[cmd.exe] )) && CMD="$(which cmd.exe)" || CMD="/mnt/c/Windows/system32/cmd.exe"
 export CMD
 
-WIN_HOME="$(${CMD} /c "<nul set /p=%UserProfile%" 2>/dev/null)"
-WIN_HOME="$(wslpath ${WIN_HOME})"
+WIN_HOME="$(wslpath $(${CMD} /c "<nul set /p=%UserProfile%" 2>/dev/null))"
 export WIN_HOME
+
+[ -L "${HOME}/win" ] && [ -e "${HOME}/win" ] || \
+	ln -s "${WIN_HOME}" "${HOME}/win"
