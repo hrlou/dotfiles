a4da5a1fd0929e140ada010c9483c692d8434e5d
Hess Lewis <hrl@hrlou.net>
2023-03-01 13:46:38 -0500

    Update .assets/install.sh Update 00-install-package-mgr.sh

--- .assets/install.sh
+++ .assets/install.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
-# Bootstrap
 eval "$(curl -fsLS git.hrlou.net/hrlou/dotfiles/-/raw/main/.assets/include.sh)"
 export BINDIR="$HOME/.local/bin"
+
 # Install Chezmoi
 if [ -f "${BINDIR}/chezmoi" ]; then
         CZ="${BINDIR}/chezmoi"
 elif command -v chezmoi >/dev/null; then
-        _log_info "chezmoi found"
+        _log_info "Chezmoi found"
         CZ="$(which chezmoi)"
 else
-        _log_info "installing chezmoi"
+        _log_info "Installing chezmoi"
         mkdir -p "$BINDIR"
         curl -fsLS get.chezmoi.io | sh
         CZ="$BINDIR/chezmoi"
 fi
-_log_debug "testing connection..."
+
+_log_debug "Testing connection..."
 REMOTE="$(ssh git@git.hrlou.net >/dev/null 2>&1 && \
         printf "git@git.hrlou.net:hrlou/dotfiles.git" || \
         printf "https://git.hrlou.net/hrlou/dotfiles.git")"
-_log_info "using '$REMOTE'"
+_log_info "Using '$REMOTE'"
 $CZ init $REMOTE
 $CZ apply
 SRC="$(${CZ} source-path)"
 . "${SRC}/.assets/include.sh"
-_log_info "dotfiles are installed"
-_log_info "please restart shell session"
+_log_info "Dotfiles are installed, please restart shell session"
