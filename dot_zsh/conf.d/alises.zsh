3f20a945de8ce8dba9fddf8fe2aaabc3bef34b74
Hess Lewis <hess@ali-lewis.com>
2023-12-22 12:47:34 -0500

    Update .zsh/conf.d/alises.zsh

--- dot_zsh/conf.d/alises.zsh
+++ dot_zsh/conf.d/alises.zsh
@@ -6,7 +6,7 @@ alias ls='ls --color=auto'
 alias ll='ls -ahlF'
 alias la='ls -A'
 alias l1='ls -1'
-alias l='ls -CF1'
+alias l='ls -CF1a'
 alias grep='grep --color=auto'
 alias h='fc -l'
 alias j=jobs
