013fd859a1406713fd7defb7427c23a40f36638d
Hess Lewis <hess@ali-lewis.com>
2024-02-05 13:16:09 -0500

    Update .config/joshuto/preview.sh

--- dot_config/joshuto/executable_preview.sh
+++ dot_config/joshuto/executable_preview.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
 shift
-file="$1"
+FILE_PATH="$1"
+PREVIEW_WIDTH=10
+PREVIEW_HEIGHT=10
 
-case $(file -b --mime-type "${file}") in
+case $(file -b --mime-type "${FILE_PATH}") in
 	text/* | application/json)
-		bat "${file}"
+		# bat "${FILE_PATH}"
+		bat \
+                	--color=always --paging=never \
+                	--style=plain \
+                	--terminal-width="${PREVIEW_WIDTH}"
 		exit 0
 		;;
 	application/x-mach-binary | application/zip)
-		ls -lha "${file}"
+		ls -lha "${FILE_PATH}"
 		exit 0
 		;;
 	*)
